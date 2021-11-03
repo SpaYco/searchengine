9aea07ccf4977050a87c1b11e51a22791d9298d6
Aziz Mejri <zizou.aziz101@gmail.com>
2021-11-03 17:14:30 +0100

    Add top search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @grouped_searches = Search.group(:query).count
-    p '=========================='
-    p @grouped_searches
-    p '=========================='
+    @grouped_searches = Search.group(:query).pluck("query, count(query) as COUNT").sort_by { |a| a[1] }.reverse.first(100)
   end
 end
