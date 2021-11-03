134d611113a50956e89633f514a5d25ef5edda31
Aziz Mejri <zizou.aziz101@gmail.com>
2021-11-03 21:34:03 +0100

    Add controller specs

---
+++ spec/controllers/article_spec.rb
@@ -0,0 +1,26 @@
+require 'capybara/rspec'
+require 'rails_helper'
+
+describe 'Articles', type: :feature do
+
+  before(:each) do |idx|
+    50.times do
+      Article.create(
+      title: "title#{idx}",
+      body: Faker::Lorem.paragraph(sentence_count: 10)
+      )
+    end
+  end
+  
+  it 'should have a title' do
+    visit '/'
+    expect(page).to have_content('title')
+  end
+
+  it 'should have a body' do
+    visit '/'
+    expect(page).to have_content('body')
+  end
+
+
+end
